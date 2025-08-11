2cb1c3f03eca19b38051c4f3ee1e21b22b31a7c3
keicreate <keicreate20240704@gmail.com>
2025-08-12 00:42:37 +0900

    コメントを新規作成できるようにしました。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,35 +1,21 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_article, only: [:new, :create, :destroy]
-
   def new
     article = Article.find(params[:article_id])
-    @comments = article.comments.build
+    @comment = article.comments.build
   end
 
   def create
-    @comment = @article.comments.new(comment_params)
-    @comment.user = current_user
-
+    article = Article.find(params[:article_id])
+    @comment = article.comments.build(comment_params)
     if @comment.save
-      redirect_to article_path(@article), notice: 'コメントが投稿されました'
+      redirect_to article_path(article), notice: 'コメントが追加されました'
     else
-      render :new
+      flash.now[:error] = 'コメントの追加に失敗しました'
+      render :new, status: :unprocessable_entity
     end
   end
 
-  def destroy
-    comment = @article.comments.find(params[:id])
-    comment.destroy!
-    redirect_to article_path(@article), status: :see_other, notice: 'コメントが削除されました'
-  end
-
   private
-
-  def set_article
-    @article = Article.find(params[:article_id])
-  end
-
   def comment_params
     params.require(:comment).permit(:content)
   end
